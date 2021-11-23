03449b85e65cbb86c10f6fc3f43c10ee1fe40450
Omito411 <ste.omi@yahoo.com>
2021-11-23 23:51:05 +0000

    index updates

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// New Toggler //
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+const navUL = document.querySelector(".nav-ul");
+
+hamburger.addEventListener("click", () => {
+    navUL.classList.toggle("show");
+    hamburger.classList.toggle("hide");
+});
+
+close.addEventListener("click", () => {
+    navUL.classList.toggle("show");
+    hamburger.classList.toggle("hide");
+});
