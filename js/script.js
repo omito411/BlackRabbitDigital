cb1991cb26123232b4c59bf608d9b960bcfdf962
Omito411 <ste.omi@yahoo.com>
2021-11-28 02:10:52 +0000

    Homepage update
    Update + addition of carousel

--- js/script.js
+++ js/script.js
@@ -12,3 +12,43 @@ close.addEventListener("click", () => {
     navUL.classList.toggle("show");
     hamburger.classList.toggle("hide");
 });
+
+// Carousel //
+let thumbnails = document.getElementsByClassName("thumbnail");
+let carousels = document.getElementById("carousel");
+let buttonRight = document.getElementById("slide-right");
+let buttonLeft = document.getElementById("slide-left");
+
+
+buttonLeft.addEventListener("click", () => {
+    carousels.scrollLeft -= 125;
+});
+
+buttonRight.addEventListener("click", () => {
+    carousels.scrollLeft += 125;
+});
+
+const maxScrollLeft = carousels.scrollWidth - carousels.clientWidth;
+//alert(maxScrollLeft);
+
+//AutoPlay carousel
+function autoPlay() {
+    if(carousels.scrollLeft > (maxScrollLeft - 1)){
+        carousels.scrollLeft -= maxScrollLeft;
+    }
+    else{
+        carousels.scrollLeft += 1;
+    }
+}
+
+let play = setInterval(autoPlay, 50);
+
+//Pause carousel while hovering
+for (let i = 0; i < thumbnails.length; i++){
+    thumbnails[i].addEventListener("mouseover", () => {
+        clearInterval(play)
+    })
+    thumbnails[i].addEventListener("mouseout", () => {
+        return play = setInterval(autoPlay, 50);
+    })
+}
