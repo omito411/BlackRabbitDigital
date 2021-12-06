951ea2bec3bbb25da2f7136c2776c0f9f323159d
Aisling Dwyer <x21123748@student.ncirl.ie>
2021-12-06 12:01:26 +0000

    Contact page form now working with validation

--- contact/script.js
+++ contact/script.js
@@ -1,8 +1,14 @@
+
+
 function formSubmission(){
  var name = document.getElementById("name").value
  var email = document.getElementById("email").value
- var companyName = document.getElementById("companyName").value
- var enquiry = document.getElementById("enquiry").value
-
- alert("Thank you "+name+" for your enquiry. We will be in contact with you shortly via the given email address, " +email);
+ 
+ window.alert("Thank you "+name+" for your enquiry. We will be in contact with you shortly via the given email address, " +email);
 }
+
+
+document.addEventListener('submit', event => {
+event.preventDefault()
+formSubmission()
+})
