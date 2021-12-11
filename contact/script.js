ac9c1dc5efc97d9004e5feb80170a4c0513e9e55
Aisling Dwyer <x21123748@student.ncirl.ie>
2021-12-11 15:44:35 +0000

    Updates to about and contact page

--- contact/script.js
+++ contact/script.js
@@ -1,14 +1,23 @@
 
 
-function formSubmission(){
- var name = document.getElementById("name").value
- var email = document.getElementById("email").value
- 
- window.alert("Thank you "+name+" for your enquiry. We will be in contact with you shortly via the given email address, " +email);
-}
-
 
 document.addEventListener('submit', event => {
 event.preventDefault()
-formSubmission()
+hideContactForm()
+
 })
+
+function hideContactForm(){
+    document.getElementById("submission-response").style.display = "none";
+    formSubmission()
+  }
+
+function formSubmission(){
+ var name = document.getElementById("name").value
+ var email = document.getElementById("email").value
+ var response = "Thank you, <span class='hyperlink-text'>"+name+"</span>, for your enquiry. <br><br>We will be in contact with you shortly to address your enquiry via the given email address, <span class='hyperlink-text'>" +email+"</span>. <br><br>To send another message, click <a class='hyperlink-text' href='contact.html'>here</a>.";
+ document.getElementById("contact-form").innerHTML += response;
+}
+
+
+
